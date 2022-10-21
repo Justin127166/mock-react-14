a4c34dc58137147f929f59b86d64ecf9396b82b3
Justin Alvarez <justinalvarez.1221@gmail.com>
2022-10-21 16:35:57 -0500

    first commit

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,7 +1,14 @@
+import { useState } from "react"
+
 const PostItem = ({post}) => {
+    const [singlePost, setSinglePost] =useState("")
+    const handleClick = (e)=>{
+        setSinglePost = e.target.id
+        console.log(e.target.id)
+    }
     return(
         <div className="listItem">
-            <h1>{post.title}</h1>
+            <h1 onClick={handleClick}>{post.title}</h1>
             <p>{post.id}</p>
         </div>
     )
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,16 @@
 import PostItem from './PostItem'
+import { useState } from 'react'
 
 const Posts = ({posts}) => {
+    const [singlePost, setSinglePost] =useState("")
+const handleClick = (e) =>{
+ setSinglePost(e.target.value)
+ console.log(e.target.value)
+
+} 
+
     return posts.map((post) => (
-        <PostItem post={post} />
+        <PostItem post={post} onClick ={handleClick}/>
     ))
 }
 
